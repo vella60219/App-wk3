33ff463c9a1f64f4d0813818e72f15c9a85de93b
vella60219 <83958906+vella60219@users.noreply.github.com>
2022-03-21 02:12:52 +0800

    剩 header

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,30 +1,199 @@
 import React from "react";
+import StarRating from 'react-native-star-rating';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import ActionButton from 'react-native-action-button';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import BookScreen from "../screens/BookScreen";
 import DetailScreen from "../screens/DetailScreen";
+import WishlistScreen from '../screens/WishlistScreen';
+import MyBooksScreen from "../screens/MyBooksScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <StackNavigator/>
+            <MyTabs/>
         </NavigationContainer>
     );
 };
 
-const StackNavigator = () => {
+
+const MyTabs = () => {
+    return (
+      <Tab.Navigator
+        initialRouteName="HomeStack"
+        screenOptions={{
+            tabBarActiveTintColor: '#6200EE',
+            tabBarInactiveTintColor: '#666666',
+          // headerShown: false
+
+            tabBarLabelStyle: {
+                fontWeight: '500',
+                fontSize: 12,
+                letterSpacing: 0.4,
+            },
+            tabBarIconStyle: {
+                marginTop: 11,
+            },
+        }}
+      >
+        <Tab.Screen 
+          name="HomeStack" 
+          component={HomeStack}
+          options={{
+            headerShown: false,
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="WishlistStack" 
+          component={WishlistScreen} 
+          options={{
+            title: "Wishlist",
+            headerTitleStyle: {
+              fontWeight: '400',
+              fontSize: 20
+            },
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="bookmark" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="MyBooksStack" 
+          component={MyBooksScreen} 
+          options={{
+            title: "My books",
+            headerTitleStyle: {
+              fontWeight: '400',
+              fontSize: 20
+            },
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="book-open" color={color} size={24} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+
+  const onStarRatingPress =(rating) => {
+    if (rating) {
+        rating =0;
+    } else {
+        rating = 1
+    }
+  }
+
+  class GeneralStarExample extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          starCount: 0
+        };
+      }
+    
+      onStarRatingPress() {
+        if (this.state.starCount) {
+            this.state.starCount =0;
+        } else {
+            this.state.starCount = 1
+        }
+      }
+    
+      render() {
+        return (
+          <StarRating
+            disabled={false}
+            iconSet={"bookmark"}
+            maxStars={1}
+            starSize={14}
+            rating={this.state.starCount}
+            selectedStar={(rating) => this.onStarRatingPress()}
+          />
+        );
+      }
+    // return(
+    //     <StarRating
+    //     disabled={false}
+    //     maxStars={1}
+    //     iconSet={"bookmark"}
+    //     starSize={14}
+    //     />
+    // );
+};
+
+const HomeStack = () => {
     return (
           <Stack.Navigator>
             <Stack.Screen
                 name="Home"
                 component={BookScreen}
+                options={{
+                    title: null,
+                }}
             />
             <Stack.Screen
                 name="Detail"
                 component={DetailScreen}
+                options={{
+                    
+
+                    title: null,
+                    headerBackTitleVisible: false,
+                    headerShadowVisible:false,
+                    //headerBackImage : "../img/icon_back",
+                    headerTintColor: '#131313',
+                    headerStyle:{
+                        elevation: 0,
+                        shadowOpacity: 0,
+                        shadowOffset:0,
+                      
+                    },
+                    headerRight:() => (
+                        // <GeneralStarExample/>
+                        // <ActionButton
+                        //     renderIcon={(active) => (
+                        //         active ? (
+                        //             <MaterialCommunityIcons name="bookmark" color={'#131313'} size={18} />
+                        //         ) : (
+                        //             <MaterialCommunityIcons name="bookmark-outline" color={'#131313'} size={18} />
+                        //         )
+                        //     )}
+                        // />
+
+
+
+                        <StarRating
+                            disabled={false}
+                            maxStars={1}
+                            rating={0}
+                            emptyStar={'bookmark-border'}
+                            fullStar={'bookmark'}
+                            iconSet={'MaterialIcons'}
+                            starSize={24}
+                            // selectedStar={(rating) => this.onStarRatingPress()}
+                            // selectedStar={
+                            //     {rating}?(rating=1):null
+                            // }
+                            />
+                    )
+
+                }}
+                
+                
             />
         </Stack.Navigator>
       );
---
+++ src/screens/MyBooksScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Center, Text } from "native-base";
+
+const MyBooksScreen = () => {
+    return (
+    <Center bg="emerald.100" flex={1}>
+        <Text fontSize={30}>
+            This is MyBooks Page
+        </Text>              
+    </Center>
+    );
+}
+
+export default MyBooksScreen;
